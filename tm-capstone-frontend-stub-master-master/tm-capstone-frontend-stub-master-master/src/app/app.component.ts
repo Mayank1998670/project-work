4bfd62f66d353e5a2070fcfd1838eb02b1496c6f
Mayank1998670 <67468694+Mayank1998670@users.noreply.github.com>
2020-10-07 10:44:50 +0530

    frontend part

---
+++ tm-capstone-frontend-stub-master-master/tm-capstone-frontend-stub-master-master/src/app/app.component.ts
@@ -0,0 +1,74 @@
+import {Component, OnInit} from '@angular/core';
+import { NgForm } from '@angular/forms';
+//import {AppDataService} from './_shared/_services/app-data.service';
+import { faSearch, faShoppingCart, faUser } from '@fortawesome/free-solid-svg-icons';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  title = 'upgrad-eshop-frontend';
+  faShoppingCart = faShoppingCart;
+  faUserIcon= faUser;
+  faSearchIcon =faSearch;
+
+  showModal: boolean;
+  // showModalOne: boolean=false;
+   submitted = false;
+   signupcard:boolean;
+   signincard:boolean= true;
+  
+   
+   email:string;
+   password:string;
+   mobileNumber:string;
+   confirmPassword:string;
+   lastName:string;
+   firstName:string;
+
+  constructor() {
+    
+ }
+//  constructor( private appDataService: AppDataService) {}
+
+
+  ngOnInit(): void {
+  //  this.appDataService.initializeApp();
+  }
+
+  closeSubMenus() {
+
+  }
+  show()
+  {
+    // Show-Hide Modal Check
+    this.showModal = true;
+    this.signincard=true;
+    this.signupcard=false;
+  }
+
+  hide()
+  {
+    this.showModal = false;
+  }
+
+  onSubmit(credentials:NgForm){
+    console.log(credentials);
+  }
+
+  signupUser(credentials:NgForm){
+    console.log(credentials);
+  }
+ 
+  onSigninClick(){
+    this.signupcard=false;
+    this.signincard=true;
+   }
+  
+  onSignupClick(){
+   this.signincard =false;
+   this.signupcard=true;
+   }
+}
---
+++ tm-capstone-frontend-stub-master-master/tm-capstone-frontend-stub-master-master/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FontAwesomeModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
